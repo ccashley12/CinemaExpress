1d40f9dc7d24ec603f283382106917813025becb
ccashley12 <ccashley12@gmail.com>
2024-08-01 15:53:49 -0400

    added files to get app started

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client';
+
+import ".index.scss";
+
+const CinemaExpressApplication = () => {
+    return (
+        <div className="cinema-express">
+            <div> Good morning</div>
+        </div>
+    );
+};
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+root.render(<CinemaExpressApplication />);
