bc8b561113672f7da76f7ba75b1523ae1fa788ea
ccashley12 <ccashley12@gmail.com>
2024-08-05 21:21:41 -0400

    updated image url for kill bill

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -50,7 +50,7 @@ export const MainView = () => {
             description: "The lead character, called 'The Bride,' was a member of the Deadly Viper Assassination Squad, led by her lover 'Bill.' Upon realizing she was pregnant with Bill's child, 'The Bride' decided to escape her life as a killer. She fled to Texas, met a young man, who, on the day of their wedding rehearsal was gunned down by an angry and jealous Bill (with the assistance of the Deadly Viper Assassination Squad). Four years later, 'The Bride' wakes from a coma, and discovers her baby is gone. She, then, decides to seek revenge upon the five people who destroyed her life and killed her baby. The saga of Kill Bill Volume I begins.",
             genre: "Thriller",
             director: "Quentin Tarantino",
-            image: "https://www.imdb.com/title/tt0266697/mediaviewer/rm2033140224/?ref_=tt_ov_i"
+            image: "https://c7.alamy.com/comp/E5MG8G/kill-bill-vol-1-us-poster-art-uma-thurman-2003-miramax-courtesy-everett-E5MG8G.jpg"
         },
         {
             id: 7,
