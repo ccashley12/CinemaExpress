dd5135ee17a094e77b661dc1d2d71ee3259ba899
ccashley12 <ccashley12@gmail.com>
2024-09-03 13:08:40 -0400

    deleted unused code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,11 +3,10 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
-import Button from "react-bootstrap/Button";
+import { ProfileView } from "../profile-view/profile-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
-import { ProfileView } from "../profile-view/profile-view";
 
 export const MainView = () => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
