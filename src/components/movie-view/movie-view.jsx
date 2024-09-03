a784fa4e8c1296bb305b8dd3a10c63c75080a50e
ccashley12 <ccashley12@gmail.com>
2024-09-03 13:06:40 -0400

    added code for bootstrap routing

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,35 +1,85 @@
+import React, {useState, useEffect } from "react";
+import { Button, Card, Row} from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom"
 import "./movie-view.scss";
 
-export const MovieView = ({ movies }) => {
+export const MovieView = ({ movies, user, token, setUser }) => {
     const { movieId } = useParams();
+    const [isFavorite, setIsFavorite] = useState(false);
     const movie = movies.find((m) => m.id === movieId);
 
+    useEffect(() => {
+        if(user && user.FavoriteMovies) {
+            const isFavorite = user.FavoriteMovies.includes(moviesId);
+            setIsFavorite(isFavorite);
+        }
+    }, [movieId, user]);
+
+    const addtoFavorite = () => {
+        fetch(`https://cinema-express-948d60ca8d20.herokuapp.com/users/${user.Username}/${movieId}`, 
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+        }).then((data) => {
+            setUser(data);
+            localStorage.setItem("user", JSON.stringify(data));
+            setIsFavorite(true);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    };
+
+    const removefromFavorite = () => {
+        fetch(`https://cinema-express-948d60ca8d20.herokuapp.com/users/${user.Username}/${movieId}`,
+        {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            if(response.ok) {
+                return response.json();
+            }
+        }).then((data) => {
+            setUser(data);
+            localStorage.setItem("user", JSON.stringify(data));
+            setIsFavorite(false);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    };
+
     return (
-        <div>
-            <div>
-                <img className="w-100" src={movie.image} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.description}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.genre}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director}</span>
-            </div>
-            <Link to={`/`}>
-                <button className="back-button">Go Back</button>
-            </Link>
-        </div>
-    );
-};
+        <Card className="h-100 w-100">
+            <Card.Img variant="top" src={movie.imagePath} />
+                <Card.Body>
+                    <Card.Header className="text-center fs-1">{movie.title}</Card.Header>
+                    <br></br>
+                        <Card.Text><strong>Director</strong> - {movie.director.name}</Card.Text>
+                        <Card.Text><strong>Genre</strong> - {movie.genre.name}</Card.Text>
+                        <Card.Text><strong>Description</strong> - {movie.description}</Card.Text>
+                    <Link to={"/"}>
+                        <button className="back-button">Back</button>
+                    </Link>
+                    <div className="mt-1">
+                        {isFavorite ? (
+                            <Button variant="danger" onClick={removefromFavorite}>Remove from favorites</Button>
+                        ) : (
+                            <Button variant="primary" onClick={addtoFavorite}>Add to favorites</Button>
+                        )}
+                    </div>
+                </Card.Body>
+        </Card>
+    )
+}
