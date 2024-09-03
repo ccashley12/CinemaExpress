1519421f384486a825030c73782e025ded1969df
ccashley12 <ccashley12@gmail.com>
2024-09-03 13:18:48 -0400

    deleted unused code

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect } from "react";
-import { Button, Card, Row} from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom"
 import "./movie-view.scss";
