28985cebcdcfdf9c8cf260f1fe3dd339976e200d
ccashley12 <ccashley12@gmail.com>
2024-09-16 17:16:22 -0400

    corrected code spelling errors

--- src/components/profile-view/profile-info.jsx
+++ src/components/profile-view/profile-info.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-export const ProfileInfo = ({ email, name }) => {
+function ProfileInfo({ email, name }) {
     return (
         <>
             <h2>Account Information</h2>
             <p>Username: {name}</p>
-            <p>Eamil: {email}</p>
+            <p>Email: {email}</p>
         </>
     );
 };
 
-ProfileInfo.prototype ={
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired
-};
+export default ProfileInfo
