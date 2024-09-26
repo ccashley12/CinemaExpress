d29e0d31f1baff23312444b49514d8615b34cf9c
ccashley12 <ccashley12@gmail.com>
2024-09-26 11:45:12 -0400

    deleted code causing error

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,149 +1,64 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Row, Col } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { ProfileView } from "../profile-view/profile-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
-import { Link } from "react-router-dom";
-import axios from "axios";
-
 
 export const MainView = () => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedUser = JSON.parse(localStorage.getItem("User"));
     const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
-    const [users,setUsers] = useState([]);
-    const [favoriteMovies,setFavoriteMovies] = useState(null);
-    const [error, setError] = useState(null);
-    const [user, setUser] = useState(storedUser);
-    const [token, setToken] = useState(storedToken);
-
-    const handleFavoriteToggle = async (movieId, isFavorite) => {
-        const storedToken = localStorage.getItem("token");
-        const username = user.username;
-
-        try {
-            const headers = {
-                Authorization: `Bearer ${storedToken}`,
-            };
-
-            if (isFavorite) {
-                await axios.post(`https://cinema-express-948d60ca8d20.herokuapp.com/users/${username}/movies/${movieId}`,
-                {},
-                { headers }
-            );
-
-            const user = JSON.parse(localStorage.getItem('user'));
-            user.favorite_movies.push(movieId);
-            localStorage.setItem('user', JSON.stringify(user));
-            setFavoriteMovies([...user.favorite_movies]);
-
-            } else {
-                await axios.delete(`https://cinema-express-948d60ca8d20.herokuapp.com/users/${username}/movies/${movieId}`,
-                { headers}
-                );
-
-                const user = JSON.parse(localStorage.getItem('user'));
-                const favorites = user.favorite_movies.filter((id) => id !== movieId);
-                user.favorite_movies = [...favorites];
-                localStorage.setItem("user", JSON.stringify(user));
-                setFavoriteMovies([...favorites]);
-            }
-        } catch (error) {
-            console.error("Error updating favorite movies:", error);
-        }
-    };
+    const [user, setUser] = useState(storedUser? storedUser : null);
+    const [token, setToken] = useState(storedToken? storedToken : null);
 
     useEffect(() => {
-        if (!token) return;
+        if(!token) {
+            return;
+        }
 
         fetch("https://cinema-express-948d60ca8d20.herokuapp.com/movies", {
             headers: { Authorization: `Bearer ${token}` }
         })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then((data) => {
-                const moviesFromApi = data.map((movie) => {
-                    return {
-                        id: movie._id,
-                        title: movie.Title,
-                        description: movie.Description,
-                        genre: movie.Genre.Name,
-                        image: movie.ImagePath,
-                        director: movie.Director.Name,
-                    };
-                });
-
-                setMovies(moviesFromApi);
-            }).catch((error) => {
-                console.error("Error loading movies:", error);
-                setError(error.message);
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesApi = data.map((movie) => {
+                return {
+                    id: movie._id,
+                    title: movie.Title,
+                    description: movie.Description,
+                    image: movie.ImagePath,
+                    genre: movie.Genre.Name,
+                    director: movie.Director.Name
+                };
             });
-        
-        fetch ("https://cinema-express-948d60ca8d20.herokuapp.com/users", {
-            headers: { Authorization: `Bearer ${token}` },
-        })
-            .then((response) => {
-                if(!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then((data) => {
-                const usersFromApi = data.map((user) => ({
-                    userId: user._id,
-                    name: user.name,
-                    username: user.username,
-                    password: user.password,
-                    email: user.email,
-                    birthday: user.birthday,
-                    favoriteMovies: user.favorite_movies || [],
-                }));
-                setUsers(usersFromApi);
-            })
-            .catch((error) => {
-                console.error("Error loading users:", error);
-                setError(error.message);
-            });
+            setMovies(moviesApi);
+        }).catch((e) => {
+            console.log(e);
+        });
     }, [token]);
 
-    if(error) {
-        return <div>Error: {error}</div>;
+    const onLoggedIn = (user, token) => {
+        setUser(user);
+        setToken(token);
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("token", token);
     }
-
-    // const onLoggedIn = (user, token) => {
-    //     setUser(user);
-    //     setToken(token);
-    //     localStorage.setItem("user", JSON.stringify(user));
-    //     localStroage.setItem("token", token);
-    // }
-
     const onLoggedOut = () => {
         setUser(null);
         setToken(null);
         localStorage.clear();
-    };
+    }
+    const updatedUser = user => {
+        setUser(user);
+        localStorage.setItem("user", JSON.stringify(user));
+    }
 
-    useEffect(() => {
-        if (user) {
-            setFavoriteMovies(users.favorite_movies);
-        }
-    }, [user]);
-    console.log(movies);
-
-    // const updatedUser = user => {
-    //     setUser(user);
-    //     localStorage.setItem("user", JSON.stringify(user));
-    // }
-
-    // if (!user) {
         return (
             <BrowserRouter>
                 <NavigationBar 
@@ -161,11 +76,6 @@ export const MainView = () => {
                                         ) : (
                                             <Col md={5}>
                                                 <SignupView />
-                                                <Col md={12} className="text-center my-3">
-                                                    <span>
-                                                        <Link to="/login">Return to Login</Link>
-                                                    </span>
-                                                </Col>
                                             </Col>
                                         )}
                                     </>
@@ -180,23 +90,15 @@ export const MainView = () => {
                                         ) : (
                                             <Col md={5}>
                                                 <LoginView
-                                                    onLoggedIn={(user, token) => {
-                                                        setUser(user);
-                                                        setToken(token);
-                                                    }}
+                                                    onLoggedIn={onLoggedIn}
                                                 />
-                                                <Col md={12} className="text-center my-3">
-                                                    <span>
-                                                        <Link to="/signup">Click HERE to Sign up</Link>
-                                                    </span>
-                                                </Col>
                                             </Col>
                                         )}                                
                                     </>
                                 }
                             />
                             <Route
-                                path="/users/:userId"
+                                path="/users/:Username"
                                 element={
                                     <>
                                         {!user ? (
@@ -204,10 +106,10 @@ export const MainView = () => {
                                         ) : (
                                             <Col md={5}>
                                                 <ProfileView
-                                                    users={users}
-                                                    favoriteMovies={favoriteMovies}
-                                                    handleFavoriteToggle={handleFavoriteToggle}
-                                                    setFavoriteMovies = {setFavoriteMovies}
+                                                    user={user}
+                                                    token={token}
+                                                    updatedUser={updatedUser}
+                                                    onLoggedOut={onLoggedOut}
                                                 />
                                             </Col>
                                         )}
@@ -224,7 +126,12 @@ export const MainView = () => {
                                             <Col>The list is empty!</Col>
                                         ) : (
                                             <Col md={8}>
-                                                <MovieView movies={movies} />
+                                                <MovieView 
+                                                    movies={movies}
+                                                    user={user}
+                                                    token={token}
+                                                    setUser={setUser}
+                                                />
                                             </Col>
                                         )}
                                     </>
@@ -240,19 +147,10 @@ export const MainView = () => {
                                             <Col>This list is empty!</Col>
                                         ) : (
                                             <>
-                                                <Row className="justify-content-md-center mt-5">
-                                                    <Col is={8} className="text-center">
-                                                        <h1>Movies</h1>
-                                                    </Col>
-                                                </Row>
-
                                                 {movies.map((movie) => (
                                                     <Col className="mb-4" key={movie.id} md={3}>
                                                         <MovieCard 
                                                             movie={movie}
-                                                            isFavorite={favoriteMovies.includes(String(movie.id))}
-                                                            onFavoriteToggle={handleFavoriteToggle}
-                                                            username={user.username}
                                                         />
                                                     </Col>
                                                 ))}
