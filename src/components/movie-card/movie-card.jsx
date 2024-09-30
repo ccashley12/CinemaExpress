bfda71ad3803f3864e8359c0bac6fbeb84da2efb
ccashley12 <ccashley12@gmail.com>
2024-09-30 18:11:46 -0400

    corrected syntax errors

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Button,  Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie }) => {
+
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={movie.image} />
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
-                <Card.Text>{movie.author}</Card.Text>
+                <Card.Text>{movie.director}</Card.Text>
                 <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
                     <Button variant="link">Open</Button> 
                 </Link>
@@ -20,8 +21,9 @@ export const MovieCard = ({ movie }) => {
 
 MovieCard.propTypes = {
     movie: PropTypes.shape({
+        id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
-        imagePath: PropTypes. string.isRequired,
+        image: PropTypes. string.isRequired,
         director: PropTypes.string.isRequired
-    }).isRequired,
+    }).isRequired
 };
