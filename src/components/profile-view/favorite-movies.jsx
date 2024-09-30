a31689c2a3bfb625d759e8721ecad5818684c65a
ccashley12 <ccashley12@gmail.com>
2024-09-30 18:12:51 -0400

    corrected some syntax errors and added bootstrap routing

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,30 +1,36 @@
-import PropTypes from "prop-types";
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { Button, Card, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const FavoriteMovies = ({ favMovies }) => {
     return (
-        <div>
-            {FavoriteMovies.length === 0 ? (
-                <p>No Favorite Movies selected</p>
-            ) : (
-                favMovies.map((movie) = (
-                    <Card key={movie._id} className="mb-3">
-                        <Card.Body>
-                            <Card.Title>{movie.title}</Card.Title>
-                            <Card.Text>{movie.director}</Card.Text>
-                            <Link to={`/movies/${movie._id}`}>
-                                <Button variant="primary">About Movie</Button>
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                ))
-            )}
-        </div>
+        <>
+            <Row>
+                <Col xs={12}>
+                    <h4>Favorite Movies</h4>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    {favMovies?.map((movie) => (
+                            <Card key={movie._id} className="mb-3">
+                                <Card.Body>
+                                    <Card.Title>{movie.title}</Card.Title>
+                                    <Card.Text>{movie.director}</Card.Text>
+                                    <Link to={`/movies/${movie._id}`}>
+                                        <Button variant="primary">About Movie</Button>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        )
+                    )}
+                </Col>
+            </Row>
+        </>
     );
 };
 
-FavoriteMovies.prototype = {
-    FavoriteMovies: PropTypes.array.isRequired
+FavoriteMovies.propTypes = {
+    favMovies: PropTypes.array.isRequired
 };
