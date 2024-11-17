dcd3085c51fb339e900787eff1e1e1da41d65aa2
ccashley12 <ccashley12@gmail.com>
2024-11-17 14:35:15 -0500

    added favorite buttons

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import "./movie-view.scss";
 
-export const MovieView = ({ movies, onAddFavorite, onRemoveFavorite }) => {
+export const MovieView = ({ movies, AddFavorite, RemoveFavorite }) => {
     const { movieId } = useParams();
     const movie = movies.find((m) => m._id === movieId);
 
@@ -11,28 +12,32 @@ export const MovieView = ({ movies, onAddFavorite, onRemoveFavorite }) => {
             <div>
                 <img className="w-100" src={movie.ImagePath} alt={movie.Title} />
             </div>
+            <br></br>
             <div>
                 <span>Title: </span>
                 <span>{movie.Title}</span>
             </div>
+            <br></br>
             <div>
                 <span>Description: </span>
                 <span>{movie.Description}</span>
             </div>
+            <br></br>
             <div>
                 <span>Director: </span>
                 <span>{movie.Director.Name}</span>
             </div>
+            <br></br>
             <div>
                 <span>Genre: </span>
                 <span>{movie.Genre.Name}</span>
             </div>
-
-            <Button variant="primary" onClick={onAddFavorite}>
+            <br></br>
+            <Button variant="primary" onClick={AddFavorite}>
                     Add to Favorites
             </Button>
 
-            <Button variant="danger" onClick={onRemoveFavorite}>
+            <Button variant="danger" onClick={RemoveFavorite}>
                     Remove from Favorites
             </Button>
 
