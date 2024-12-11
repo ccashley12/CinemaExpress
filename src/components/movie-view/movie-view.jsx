156c35058ff8da1a5d8943e44d8f83692949dd20
ccashley12 <ccashley12@gmail.com>
2024-12-11 13:58:45 -0500

    added bootstrap for cohesive view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import { Row, Col, Button } from "react-bootstrap";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
@@ -7,35 +8,42 @@ export const MovieView = ({ movies }) => {
     const movie = movies.find((m) => m._id === movieId);
 
     return (
-        <div>
-            <div>
-                <img className="w-100" src={movie.ImagePath} alt={movie.Title} />
-            </div>
-            <br></br>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <br></br>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <br></br>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director.Name}</span>
-            </div>
-            <br></br>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-            </div>
-            <br></br>
-
-            <Link to={'/'}>
-                <button className="back-button">Back</button>
-            </Link>
-        </div>
+        <>
+            <Row className="mb-4">
+                <Col md={6}>
+                    <img className="w-100" src={movie.ImagePath} alt={movie.Title} />
+                </Col>
+                <Col md={6}>
+                    <Row className="mb-3">
+                        <Col> 
+                            <h2>Movie Details</h2>
+                        </Col>
+                    </Row>
+                    <Row className="mb-2">
+                        <Col xs="auto" className="font-weight-bold">Title:</Col>
+                        <Col>{movie.Title}</Col>
+                    </Row>
+                    <Row className="mb-2">
+                        <Col xs="auto" className="font-weight-bold">Description:</Col>
+                        <Col>{movie.Description}</Col>
+                    </Row>
+                    <Row className="mb-2">
+                        <Col xs="auto" className="font-weight-bold">Director:</Col>
+                        <Col>{movie.Director.Name}</Col>
+                    </Row>
+                    <Row className="mb-2">
+                        <Col xs="auto" className="font-weight-bold">Genre:</Col>
+                        <Col>{movie.Genre.Name}</Col>
+                    </Row>
+                    <Row>
+                        <Col className="text-left">
+                            <Link to={'/'}>
+                                <Button className="back-button">Back</Button>
+                            </Link>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </>
     );
 };
